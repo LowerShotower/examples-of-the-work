c4f3887a4d9ab03bc96978308b77f087660390e2
ArtemZgh <lowershotower@gmail.com>
2018-04-09 22:37:14 +0200

    make function implementation

---
+++ code-jam1/make.js
@@ -0,0 +1,31 @@
+function make(...arr) {
+     var tempArr = [];
+     tempArr.push(arr[0]);
+     var summ = 0;
+     function tempF(...nextArr) {
+          if (typeof(nextArr[0]) != "function") {
+               for (let i = 0; i < nextArr.length; i++) {
+                    tempArr.push(nextArr[i]);
+                 }
+               return tempF;
+          } else {
+               for (let i = 0; i < tempArr.length; i++) {
+                    summ += tempArr[i];
+               }
+               return summ;
+          }
+        }
+
+     tempF.toString = function() {
+     return summ;
+     };
+
+     return tempF;
+}
+
+function sum(a, b) {
+     return a + b;
+ }
+ 
+console.log(make(15)(34, 21, 666)(41)(sum));
+
